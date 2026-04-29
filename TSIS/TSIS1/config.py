131a508e341d5b69c71af18216986de024695f0c
rvuvnuly <b_kutymov@kbtu.kz>
2026-04-30 02:09:57 +0500

    add TSIS

---
+++ TSIS/TSIS1/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "phonebook",   # реальное название БД
+    "user": "nurbol",          # твоё системное имя
+    "password": "",            # если пароля нет — пустая строка
+    "port": 5432
+}
